72bc98fc24dc49a04d3dc8d1d1eb4ad5c7376db7
Daniel <filind85@gmail.com>
2019-06-14 12:27:42 +0300

    now it is possible to create world in different sizes

---
+++ mineCraft/index.js
@@ -0,0 +1,166 @@
+let parent = $('.parent-div');
+
+// this function creates a random world;
+createWorld = (xAxis) => {
+    let randomNumTree = Math.ceil(Math.random() * 6 +2);
+for (let i = 0; i < xAxis; i++) {
+    let randomNumBranches = Math.ceil(Math.random() * 2);
+    for (let x = 0; x < 20; x++) {
+        let randomNumGold = Math.ceil(Math.random() * 40);
+        let randomNumStone = Math.ceil(Math.random() * 4);
+        let el = $('<div/>');
+        el.addClass('game-tile');
+        if (i < (xAxis*0.7)) {
+            el.addClass('sky');
+            el.css({
+                width: '5%',
+                height: '5%',
+            });
+        } else if (i >= (xAxis*0.7) && randomNumGold % 40 === 0) {
+            el.addClass('gold');
+            el.css({
+                width: '5%',
+                margin: '0',
+                height: '5%',
+            });
+        } else if (i >= (xAxis*0.7) && randomNumStone % 4 === 0) {
+            el.addClass('smallRock');
+            el.css({
+                width: '5%',
+                margin: '0',
+                height: '5%',
+            });
+        } else if (i >= (xAxis*0.7)) {
+            el.addClass('dirt');
+            el.css({
+                width: '5%',
+                margin: '0',
+                height: '5%',
+            });
+        }
+        if (i >= (xAxis*0.5) && i <= (xAxis*0.7)) {
+            if (x % randomNumTree === 0) {
+                el.addClass('tree');
+                el.css({
+                    width: '5%',
+                    margin: '0',
+                    height: '5%',
+                });
+            } else {
+                el.addClass('sky');
+                el.css({
+                    width: '5%',
+                    margin: '0',
+                    height: '5%',
+                });
+            }
+        }
+        if (i >= (xAxis*0.30) && i <= (xAxis*0.5)) {
+            if (
+                x % randomNumTree === 0 ||
+                x % randomNumTree === randomNumBranches - 1 ||
+                x % randomNumTree === randomNumTree - randomNumBranches||
+                x % randomNumTree === randomNumBranches -2
+            ) {
+                el.addClass('treetop');
+                el.css({
+                    width: '5%',
+                    margin: '0',
+                    height: '5%',
+                });
+            } else {
+                el.addClass('sky');
+                el.css({
+                    width: '5%',
+                    margin: '0',
+                    height: '5%',
+                });
+            }
+        }
+        if (i <= (xAxis*0.3)) {
+            el.addClass('sky');
+            el.css({
+                width: '5%',
+                margin: '0',
+                height: '5%',
+            });
+        }
+        parent.append(el);
+    }
+}
+}
+
+// sends the harvested element to stock
+sendToStock = stock => {
+    $('#inventory').empty();
+    $('#inventory').text('product');
+    let el = $('<div/>');
+    el.addClass(stock);
+    el.addClass('my-inventory');
+    el.css({
+        width: '5%',
+        height: '50%',
+        display: 'inline-block',
+    });
+    $('#inventory').append(el);
+        //this part appends the harvested element back to the game-world; 
+    $('.my-inventory').click(() => {
+        $('.game-tile')
+            .off('click')
+            .on('click', function(e) {	
+                let myTile = e.target;
+                myTile.classList.add(stock);
+                stock = '';
+                $('#inventory').empty();
+            });
+    });
+};
+
+
+// harvest mode function; also calls the functions that will send the harvested piece to storage. 
+harvestMode = (myClass, myClass2) => {
+    $('.game-tile')
+        .off('click')
+        .on('click', function(e) {
+            if (myClass === e.target.classList[2]) {
+                sendToStock(myClass);
+            }else if (myClass2 === e.target.classList[2]) {
+                sendToStock(myClass2);
+            }
+            e.target.classList.remove(myClass, myClass2);
+        });
+};
+
+// event listeners of the different tooles that we use to harvest our products. 
+
+$('#axeImg').click(() => {
+    harvestMode('tree', 'treetop');
+    $('#axeImg').css('background-color', 'yellow');
+    $('#shovel').css('background-color', 'transparent');
+    $('#mineAxe').css('background-color', 'transparent');
+});
+
+$('#shovel').click(() => {
+    harvestMode('dirt');
+    $('#shovel').css('background-color', 'blue');
+    $('#mineAxe').css('background-color', 'transparent');
+    $('#axeImg').css('background-color', 'transparent');
+});
+
+$('#mineAxe').click(() => {
+    harvestMode('smallRock', 'gold');
+    $('#mineAxe').css('background-color', 'purple');
+    $('#shovel').css('background-color', 'transparent');
+    $('#axeImg').css('background-color', 'transparent');
+});
+
+// generates world
+
+$('.world-btn').click( () => {
+    let x = $('.x-axis').val();
+    parent.empty();
+    createWorld(x);
+})
+
+// generates a world on page load; 
+$( window ).load(createWorld(20))
